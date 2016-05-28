c0856e85121a79962f31367c1d96ca29c8e8e267
Philip A. Nunoo <ketay99@gmail.com>
2016-05-28 10:57:59 +0000

    sd

--- server/api/request.js
+++ server/api/request.js
@@ -2,9 +2,30 @@ import { Api } from './_api.js';
 import { check } from 'meteor/check';
 
 Api.addRoute('user/request', {authRequired: false}, {
-    get: function () {
-        
+    post: function () {
+        doc = this.bodyParams;
+        Requests.insert(doc);
+
         
         return {success: true};
     }
 });
+
+function registerUser (user) {
+	 // body...  
+}
+
+function get (argument) {
+	 // body...  
+}
+
+/*
+ * functions
+ * 1. Get Routes
+ * 2. Get From and to
+ * 3. Fare
+ */
+
+function getFare (from_terminal, to_terminal) {
+	let fare = 0;	 
+}
--- server/main.js
+++ server/main.js
@@ -1,5 +1,29 @@
 import { Meteor } from 'meteor/meteor';
 
+function populateRoutes (argument) {
+	if (Routes.find().length <= 0){
+
+	}
+}
 Meteor.startup(() => {
-  // code to run on server at startup
+  // code to run on server at 
+  populateRoutes()
 });
+
+
+Meteor.methods({
+	parseUpload( data ) {
+		check( data, Array );
+
+		for ( let i = 0; i < data.length; i++ ) {
+			let item   = data[ i ],
+          		exists = Routes.findOne( { from_terminal: item.from_terminal, to_terminal: item.to_terminal } );
+
+          	if ( !exists ) {
+          		Routes.insert( item );
+          	} else {
+          		console.warn( 'Rejected. This item already exists.' );
+          	}
+          }
+      }
+  });
